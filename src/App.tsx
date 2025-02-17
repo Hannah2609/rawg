18ea5d6432124872438907126e3e492525876157
Hannah2609 <hann7419@stud.kea.dk>
2025-02-17 15:34:56 +0100

    gamecards and genrelist created

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
-import NavBar from './components/NavBar';
-import './App.css'
+import GenreList from "./components/GenreList";
 
 function App() {
-
   return (
-    <Grid templateAreas={{
-      base: `"header" "main"`,
-      lg: `"header header" "aside main"`
-    }}>
+    <Grid
+      templateAreas={{
+        base: `"header" "main"`,
+        lg: `"header header" "aside main"`,
+      }}
+      templateColumns={{ base: "1fr", lg: "200px 1fr" }}
+    >
       <GridItem pl="2" area={"header"}>
-        <NavBar /> 
+        <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem pl="2" bg="pink.300" area={"aside"}>
-          aside
+        <GridItem pl="2" area={"aside"}>
+          <GenreList />
         </GridItem>
       </Show>
-      <GridItem>
+      <GridItem pl="2" area={"main"}>
         <GameGrid />
       </GridItem>
-    </Grid>  
-  )
+    </Grid>
+  );
 }
 
-export default App
+export default App;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,19 +1,37 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-    const { games, error } = useGames();
+  const skeletons = [...Array(20).keys()];
 
-    return (
-        <>
-        {error && <Text color="tomato">{error}</Text>}
-        <ul>
-            {games.map((game) => (
-            <li key={game.id}>{game.name}</li>
+  const { games, error, isLoading } = useGames();
+
+  return (
+    <>
+      {error && <Text color="tomato">{error}</Text>}
+
+      <SimpleGrid
+        columns={{ base: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={4}
+        padding="10"
+      >
+        {isLoading
+          ? skeletons.map((skeleton) => (
+              <GameCardContainer key={skeleton}>
+                <GameCardSkeleton />
+              </GameCardContainer>
+            ))
+          : games.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
             ))}
-        </ul>
-        </>
-    );
+      </SimpleGrid>
+    </>
+  );
 };
 
 export default GameGrid;
